14644ebe95eaaa135ca94ccf2ec3e7a6e055b3fc
Chenrui-Cai <cai44@illinois.edu>
2026-02-10 02:32:00 +0000

    finished

--- src/solution.cc
+++ src/solution.cc
@@ -2,5 +2,24 @@
 
 Student ReadStudentRecFromStream(std::istream& is) {
   // your implementation here...
-  return Student{};
+  std::string first_name, last_name;
+  char comma = '\0';
+  unsigned int uin = 0;
+  double gpa = 0.0;
+  is >> first_name >> last_name >> uin >> comma >> gpa;
+  if (is.fail()) {
+    return Student{};
+  }
+  if (comma != ',') {
+    return Student{};
+  }
+  if (last_name.back() != ',') {
+    return Student{};
+  }
+
+  last_name.pop_back();
+
+  Student student{first_name + " " + last_name, uin, gpa};
+
+  return student;
 }
